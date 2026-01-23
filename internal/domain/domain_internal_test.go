80e93354b25bc3fdd5aa672353befc8f7a46ed76
spcameron <cameron.seanpatrick@gmail.com>
2026-01-23 10:01:17 -0500

    Add Roster.ProjectThrough()
    The ProjectThrough method is defined against the Roster struct, and it
    accepts a time.Time cutoff point, creates an empty RosterView instance
    with a corresponding TeamID to the Roster and an EffectiveAt time
    equivalent to the argument passed. It then iterates across the
    Roster.EventHistory, and for any events within the view window, calls
    RosterView.Apply(). It ends by returning the RosterView.
    
    domain_internal_test.go is created to allow for an unexported
    unknownRosterEvent type that can be used to assert panic cases on
    Apply() and ProjectThrough().
    
    In roster_view.go, a new sentinel error ErrUnrecognizedRosterEvent is
    defined, and the previously unexported playerOnRoster() is now exported
    as PlayerOnRoster().

---
+++ internal/domain/domain_internal_test.go
@@ -0,0 +1,86 @@
+package domain
+
+import (
+	"testing"
+	"time"
+
+	"github.com/spcameron/dugout/internal/testutil/assert"
+	"github.com/spcameron/dugout/internal/testutil/require"
+)
+
+type unknownRosterEvent struct {
+	TeamID      TeamID
+	EffectiveAt time.Time
+}
+
+func (e unknownRosterEvent) isDomainEvent() {}
+func (e unknownRosterEvent) Team() TeamID {
+	return e.TeamID
+}
+func (e unknownRosterEvent) OccurredAt() time.Time {
+	return e.EffectiveAt
+}
+
+var todayLock = time.Date(
+	1986,
+	time.October,
+	26,
+	0, 0, 0, 0,
+	time.UTC,
+)
+
+const teamA = TeamID(999)
+
+func TestRosterViewApply(t *testing.T) {
+	t.Run("apply panics if roster event is unrecognized", func(t *testing.T) {
+		rv := RosterView{
+			TeamID:           teamA,
+			Entries:          nil,
+			EffectiveThrough: todayLock,
+		}
+
+		startingTeamID := rv.TeamID
+		startingLength := len(rv.Entries)
+		startingLock := rv.EffectiveThrough
+
+		e := unknownRosterEvent{
+			TeamID:      teamA,
+			EffectiveAt: todayLock,
+		}
+
+		fn := func() { rv.Apply(e) }
+
+		err := require.PanicsError(t, fn)
+		require.NotNil(t, err)
+		require.ErrorIs(t, err, ErrUnrecognizedRosterEvent)
+
+		assert.Equal(t, rv.TeamID, startingTeamID)
+		assert.Equal(t, len(rv.Entries), startingLength)
+		assert.Equal(t, rv.EffectiveThrough, startingLock)
+	})
+}
+
+func TestRosterProjectThrough(t *testing.T) {
+	t.Run("panics if roster event is unrecognized", func(t *testing.T) {
+		r := Roster{
+			TeamID: teamA,
+			EventHistory: []RosterEvent{
+				AddedPlayerToRoster{
+					TeamID:      teamA,
+					PlayerID:    1,
+					EffectiveAt: todayLock,
+				},
+				unknownRosterEvent{
+					TeamID:      teamA,
+					EffectiveAt: todayLock,
+				},
+			},
+		}
+
+		fn := func() { _ = r.ProjectThrough(todayLock) }
+
+		err := require.PanicsError(t, fn)
+		require.NotNil(t, err)
+		require.ErrorIs(t, err, ErrUnrecognizedRosterEvent)
+	})
+}
